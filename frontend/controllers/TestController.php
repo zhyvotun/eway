34e93de624e7d07b2ebd6115ad4fd5bacb117489
Zhyvotun Filip <zhyvotun@gmail.com>
2018-10-25 17:00:45 +0300

    25.10.18

---
+++ frontend/controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionIndex(){
+        return $this->render('index');
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: zhyvotun
+ * Date: 25.10.18
+ * Time: 16:49
+ */
---
+++ frontend/views/test/index.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Hello';
+/**
+ * Created by PhpStorm.
+ * User: zhyvotun
+ * Date: 25.10.18
+ * Time: 16:52
+ */
